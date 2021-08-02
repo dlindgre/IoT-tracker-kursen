4f9d9c211b1c1f2eb69a4712e098841f0faed72d
dlindgre <84927529+dlindgre@users.noreply.github.com>
2021-08-02 21:09:22 +0200

    Add files via upload
    Work in progress

--- main.py
+++ main.py
@@ -1,59 +1,154 @@
 import socket
 import time
 import pycom
+import ujson
 import ubinascii
 import machine
 import network
-#from network import LoRa
+import utime
+import uos
+import math
+import gc
+from network import WLAN
 from L76GNSV4 import L76GNSS
 from pycoproc_2 import Pycoproc
 from LIS2HH12 import LIS2HH12
-from machine import ADC
+from machine import SD
+from machine import RTC
 
-
-
-print("Före")
+gc.enable()
+#
+# Initiate sensors and SD card
+#
+print("Initiate sensor and GPS")
 py = Pycoproc()
-print("Efter")
 L76 = L76GNSS()
 L76.setAlwaysOn()
 acc = LIS2HH12(py)
-# make the socket non-blocking
-# (because if there's no data received it will block forever...)
-##s.setblocking(False)
-print("Pre fix")
+rtc=RTC()
+print("Sensors and GPS initiated")
+print("Get GPS connected")
 L76.get_fix(debug=False)
-print("post fix")
+print("GPS connected")
 if L76.fixed():
     pycom.rgbled(0x000f00)
+print("Init SD card")
+sd=SD()
+print("mounting SD card")
+uos.mount(sd, '/sd')
+uos.listdir('/sd')
+
+
+print('File session 2')
+with open ('/sd/fileWSD.txt', 'w') as f:
+    f.write("Hello!")
+with open ('/sd/fileWSD.txt') as f:
+    print(f.read())
+
+#Kolla att SD kortet är skrivbart!
+print("File session3")
+#
+# set initial values
+#
+okToStore=True
+speedPrevious=0.0
+accXPrevious=0.0
+
+calcSpeed=0.0
+#deltaTime=0 not needed
+RTCtime=rtc.now()
+print(RTCtime)
+secondsTick=RTCtime[3]*3600+RTCtime[4]*60+RTCtime[6]
+
+secondsPrevious=RTCtime[4]
+
+
+#
+# Main loop
+#
 while True:
+    break
+    #
+    # read data from sensord
+    #
     coord = L76.coordinates(debug=False)
     ido = L76.getUTCDateTime(debug=False)
     accel = acc.acceleration()
-    speed=L76.get_speed()
-    battery = py.read_battery_voltage()
+    accX=accel[0]/9.81
+    print(accel, accX, accY)
+    accY=accel[1]/9.81
+    accZ=accel[2]/9.81
+    # Compare acceleration. Do we need to store or can we use previous datapoint?
+    if abs(accX-accXPrevious)<0.05:
+        okToStore=False
+    else:
+        okToStore=True
+    if abs(accX)<0.2: # reduce noise (Experimental!)
+        accX=0
+    print(accX)
+    accXPrevious=accX
+    speedArr=L76.get_speed()
+    battery_voltage = py.read_battery_voltage()
+    lat=coord.get('latitude')
+    long=coord.get('longitude') 
+    speed=speedArr.get('speed')
+    # Compare speed. Do we need to store or can we use previous datapoint?
+    if abs(speed-speedPrevious)<3:
+        okToStore=False
+    else:
+        okToStore=True
+    speedPrevious=speed
 
-    print(coord)
-    print(speed)
+    myDate=ido.split("T")[0]
+    myTime=ido.split("T")[1]
 
-    dict = {}
-    dict['deviceToken'] = "d4c203ae-0429-4647-b377-f7524aa0707b"
-    dict['event'] = "Datacake"
-    dict['signal'] = "2"
-    dict['payload'] = {
-        'voltage': battery,
-        'speed': speed,
-        "COG": 160.89,
-        "ACC":(0.1253662, -0.07019043, 0.9925537),
-        "Coord": coord,
-        "latitude": 56.27001,
-        "longitude": 12.97098,
-        "ttf": 0,
-        "TIME":0, 
+    #function time to seconds
+    hour=myTime.split
+
+    RTCtime=rtc.now()
+    print(RTCtime)
+
+    deltaTime=RTCtime[4]-secondsPrevious
+
+
+    calcSpeed=speedPrevious+accX*deltaTime
+    print("Calculated speed: "+str(calcSpeed))
+
+
+    #
+    # Prepare dataset for upload
+    #
+    dict = {}  
+    dict = {"myTime":myTime,
+         "myDate":myDate,
+         "Voltage_battery": str(battery_voltage),
+         "latitude":str(lat),
+         "longitude":str(long),
+         "speed": str(speed),
+         "accX":str(accel[0]),
+         "accY":str(accel[1]),
+         "accZ":str(accel[2]),
     }
-   
-    print(str(dict))
     
-    pybytes.send_signal(2,str(dict))
+    
+    #
+    # if connected, write to pybytes using http protocol. If not connected, write to SD card
+    # 
+    if okToStore:
+        if wlan.isconnected():
+            pybytes.send_signal(2,ujson.dumps(dict))
+            print(ujson.dumps(dict))
+        else:
+            Print("Write to file!")
+    else:
+        Print("No need to save data")
 
-    time.sleep(10)
+
+
+    print()
+    pybytes.send_signal(2,ujson.dumps(dict)) #om kontakt saknas, skriv till fil
+
+    time.sleep(1) # No action for 1 second
+    #
+    # if no OK2Write for more than 600 iterations, go sleep
+    #
